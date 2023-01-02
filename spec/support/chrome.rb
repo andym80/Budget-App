c209c3fd42e2b19ef5825f85532ff0ca11582cdc
andym80 <andymenutti@gmail.com>
2023-01-02 14:33:16 -0300

    add rspec

---
+++ spec/support/chrome.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+	if ENV["SHOW_BROWSER"] == "true"
+	  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+	else
+	  driven_by :selenium_headless, using: :chrome, screen_size: [1400, 1400]
+	end
+  end
+end
